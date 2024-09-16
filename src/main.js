13ee08e38a0d603e3296fc8d8dadc7d9136ebb37
Anastasiia Yerashova <yerashova.a@gmail.com>
2024-09-16 16:15:16 +0200

    done

--- src/main.js
+++ src/main.js
@@ -1,14 +1,25 @@
-const startBtn = document.querySelector(".start-btn");
-const container = document.querySelector(".container");
-const result = document.querySelector(".result");
-const items = document.querySelectorAll(".item");
-console.log(items)
+fetch("https://jsonplaceholder.typicode.com/posts").then(response => response.json()).then(json => { 
+    const ul = document.querySelector(".posts-list");
+   
+      json.forEach((item, index) => { 
+        const post = document.createElement("li");
+        const postIndex = document.createElement("span");
+      const postTitle = document.createElement("h4");
+        const postText = document.createElement("p");
+        const link = document.createElement("a");
+        
+        post.classList.add("posts-list-item");
+        postIndex.classList.add("post-index");
+         postTitle.classList.add("post-title");
+        postText.classList.add("post-text");
+        link.classList.add("post-link");
 
+        postIndex.innerHTML = index + 1;
+        postTitle.innerHTML = item.title;
+        postText.innerHTML = item.body;
+        link.innerHTML = "Read more";
 
-function showOn(array) {
-    for (let item of items) {
-   item.textContent = "🤑";
-    }
-
-} 
-showOn(items)
+        post.append(postIndex, postTitle, postText, link);
+        ul.append(post);
+    })
+})
